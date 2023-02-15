ca0c28c13e45c736a3e1965c5c62a8a267533614
kiwa <kiwa@fresnou.org>
2023-02-15 09:42:02 -0800

    commit conditional

---
+++ inputs.js
@@ -0,0 +1,41 @@
+const readLine = require('readline-sync');
+
+            let answer = readLine.question("Pick a number\nYour number: ");
+            if (answer % 2 == 0){
+                console.log("divisible by  2")
+            }
+            else {
+                console.log("not divisible by 2")
+            }
+            if (answer % 4 == 0){
+                console.log("divisible by  4")
+            }
+            else {
+                console.log("not divisible by 4")
+            }
+
+            if (answer % 3 == 0){
+                console.log("divisible by  3")
+            }
+            else {
+                console.log("not divisible by 3")
+            }
+            if (answer % 5 == 0){
+                console.log("divisible by  5")
+            }
+            else {
+                console.log("not divisible by 5")
+            }
+            if (answer % 6 == 0){
+                console.log("divisible by  6")
+            }
+            else {
+                console.log("not divisible by 6")
+            }
+
+
+
+
+
+
+
