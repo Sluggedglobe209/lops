324456f7b96f089eb259575f9f6c3c095ba4568e
kiwa <kiwa@fresnou.org>
2023-02-23 10:29:29 -0800

    initial commit

--- inputnotes.js
+++ inputnotes.js
@@ -142,3 +142,26 @@ while (z < 100){
     z++;
 
 }
+if(start < 10){
+
+    console.log(start);
+
+} else if (start > 25){
+
+    console.log(start);
+
+}
+
+start++;
+
+}
+
+let answer = readLine.question("Enter y or n: ");
+
+while (answer != "y" && answer != "n"){
+
+    console.log("invalid choice");
+
+    answer = readLine.question("Enter y or n: ");
+
+}
