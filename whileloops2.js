b5fa5b8a15b100f85a7d601634d821b49d221942
kiwa <kiwa@fresnou.org>
2023-02-23 10:30:52 -0800

    initial commit

---
+++ whileloops2.js
@@ -0,0 +1,24 @@
+const readLine = require(`readline-sync`);
+let start=5
+if (start < 10){
+
+    console.log(start);
+
+} else if (start > 25){
+
+    console.log(start);
+
+}
+
+start++;
+
+
+
+let answer = readLine.question("Enter y or n: ");
+
+while (answer != "y" && answer != "n"){
+    console.log("invalid choice");
+
+    answer = readLine.question("Enter y or n: ");
+
+}
