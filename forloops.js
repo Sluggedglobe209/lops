fa1fa8a2949e3dc94ff32342c3880aba258c58dd
kiwa <kiwa@fresnou.org>
2023-02-23 10:29:49 -0800

    initial commit

---
+++ forloops.js
@@ -0,0 +1,31 @@
+//for loops
+// for (let i =0; i<= 10; i++){
+// //     console.log ("hi");
+// // }
+
+
+// //1
+//  for (let i =0; i<= 150; i++){
+//      // if (i % 2 ==0) {
+//          console.log(i);
+//      // }
+//  }
+
+
+
+
+// let sum = 0;
+// for (let i =0; i <= 100; i++){
+//     if (i % 2 == 0){
+//         sum += i;
+//     }
+// }
+// console.log(`sum: ${sum}`);
+
+//2
+
+  for (let i =0; i<= 40; i++){
+       if (i % 2 ==0) {
+          console.log(i);
+       }
+  }
