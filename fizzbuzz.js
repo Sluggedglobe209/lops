b282180db02232e68dab53f42e68747920f32f5b
kiwa <kiwa@fresnou.org>
2023-03-07 10:30:15 -0800

    initial

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+const readLine = require(`readline-sync`);
+let i = 0;
+
+while (i < 31){
+    if (i % 3 == 0){
+        if (i % 5 == 0)
+        console.log('fizz');
+        console.log('buzz');
+    }
+
+
+    i++;
+
+
+
+}
